e76b50589ad982610e5125239d3e4adb2e5b6502
ZIYAN137 <zi_yan137@outlook.com>
2024-12-22 00:50:07 +0800

    async_file

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ pub mod io;
 pub mod executor;
 pub mod block_on;
 mod utils;
+pub mod async_file;
 
 pub use block_on::block_on;
 pub use executor::Executor;
 pub use io::Stdin;
 pub use proactor::Proactor;
+pub use async_file::AsyncFile;
--- src/proactor.rs
+++ src/proactor.rs
@@ -46,4 +46,34 @@ impl Proactor {
             Poll::Pending
         }).await
     }
+
+    ///
+    pub async fn write(&self, fd: RawFd, buf: &[u8]) -> io::Result<usize> {
+        {
+            let uring = self.uring.clone();
+            let entry = opcode::Write::new(types::Fd(fd), buf.as_ptr(), buf.len() as _).build();
+            let mut uring_guard = uring.lock().unwrap();
+            unsafe {
+                uring_guard.submission().push(&entry).expect("Failed to submit request");
+            }
+            uring_guard.submit().expect("Failed to submit io_uring");
+        }
+
+        poll_fn(|cx| {
+            let mut uring_guard = self.uring.lock().unwrap();
+            if let Some(cqe) = uring_guard.completion().next() {
+                let result = cqe.result();
+                if result < 0 {
+                    return Poll::Ready(Err(io::Error::from_raw_os_error(-result)));
+                } else {
+                    return Poll::Ready(Ok(result as usize));
+                }
+            }
+            let waker = cx.waker().clone();
+            uring_guard.submit_and_wait(1).expect("Failed to wait");
+            waker.wake();
+
+            Poll::Pending
+        }).await
+    }
 }
